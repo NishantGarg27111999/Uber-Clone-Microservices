75903eef14adb00c3bbfe5103f12d6da5713c5b3
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2025-08-21 18:24:04 +0530

    Update app.js

--- Backend/User_service/app.js
+++ Backend/User_service/app.js
@@ -9,7 +9,11 @@ const connectToDb=require('./db/db');
 
 connectToDb();
 const port=process.env.PORT || 4001;
-app.use(cors());
+app.use(cors({
+  origin: '*', // or specify your frontend origin
+  methods: ['GET', 'POST'],
+  credentials: false
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
