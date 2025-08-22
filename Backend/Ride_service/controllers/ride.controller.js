5b117e64ebd0d5c948c2ae0a6c30104764ab52ea
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2025-08-22 10:55:48 +0530

    Update ride.controller.js

--- Backend/Ride_service/controllers/ride.controller.js
+++ Backend/Ride_service/controllers/ride.controller.js
@@ -58,6 +58,7 @@ module.exports.getFare=async(req,res)=>{
     const errors=validationResult(req);
 
     if(!errors.isEmpty()){
+        console.log(errors);
         return res.status(400).json({errors: errors.array()});
     }
     try{
