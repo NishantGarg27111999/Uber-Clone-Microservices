502633676b4481d0c114e4506b9dba096f34b751
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2025-08-22 14:06:03 +0530

    Update app.js

--- Backend/User_service/app.js
+++ Backend/User_service/app.js
@@ -17,8 +17,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use('/',userRoutes)
 app.get('/health',(req,res)=> res.send("ok"));
+app.use('/',userRoutes)
+
 
 
 
