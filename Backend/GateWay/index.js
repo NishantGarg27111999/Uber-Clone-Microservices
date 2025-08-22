5a8aa29988e8a47bcacb390183b982e425338423
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2025-08-22 13:54:00 +0530

    Update index.js [ added /health route]

--- Backend/GateWay/index.js
+++ Backend/GateWay/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use('/user',proxy('https://uberclone-user-service.onrender.com'));
 app.use('/captains',proxy('https://uberclone-captain-service.onrender.com'));
 app.use('/ride',proxy('https://uberclone-ride-service.onrender.com'));
+app.get('/health',(req,res)=> res.send("ok"));
 
 const port=process.env.PORT || 4000;
 server.listen(port,()=>{
