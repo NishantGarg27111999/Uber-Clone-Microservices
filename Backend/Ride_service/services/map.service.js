45189dac494a5c9ab117b5448f47b1b4221e776c
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2026-08-23 14:38:45 +0530

    Update map.service.js

--- Backend/Ride_service/services/map.service.js
+++ Backend/Ride_service/services/map.service.js
@@ -38,7 +38,7 @@ module.exports.getDistanceTime=async(origin,destination)=>{
 
     try{
         const response=await axios.get(url);
-        if(responsestatus==='OK'){
+        if(response.status==='OK'){
             // console.log(response);
             // if(response.data.rows[0].elements[0].status==='ZERO_RESULTS'){
             //     throw new Error('No routes found');
